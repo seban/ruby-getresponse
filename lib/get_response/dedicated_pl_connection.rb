dc9ed2dce8f9257d4208a248fb28651bf37bc1da
Sebastian Nowak <sebastian.nowak@gmail.com>
2013-05-13 20:01:13 +0200

    Add some documentation about type of API and connections

--- lib/get_response/dedicated_pl_connection.rb
+++ lib/get_response/dedicated_pl_connection.rb
@@ -2,6 +2,7 @@
 
 module GetResponse
 
+  # Class simulates connection to API of getresponse360.pl service.
   class DedicatedPlConnection < Connection
 
     API_URI = "http://api.getresponse360.pl/CRYPTO"
--- lib/get_response/dedicated_us_connection.rb
+++ lib/get_response/dedicated_us_connection.rb
@@ -2,6 +2,7 @@
 
 module GetResponse
 
+  # Class simulates connection to API of getresponse360.com service.
   class DedicatedUsConnection < Connection
 
     API_URI = "http://api.getresponse360.com/CRYPTO"
