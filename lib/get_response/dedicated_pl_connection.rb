11bfb771380f47e2b6f8abb713603f073a6f7cec
Sebastian Nowak <sebastian.nowak@gmail.com>
2013-05-13 19:55:36 +0200

    Introduce GetResponse::DedicatedUsConnection and GetResponse::DedicatedPlConnection.
    This two classes handle communication for GetResponse 360 clients api.

---
+++ lib/get_response/dedicated_pl_connection.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+module GetResponse
+
+  class DedicatedPlConnection < Connection
+
+    API_URI = "http://api.getresponse360.pl/CRYPTO"
+
+  end
+
+end
+
---
+++ lib/get_response/dedicated_us_connection.rb
@@ -0,0 +1,12 @@
+# encoding: utf-8
+
+module GetResponse
+
+  class DedicatedUsConnection < Connection
+
+    API_URI = "http://api.getresponse360.com/CRYPTO"
+
+  end
+
+end
+
